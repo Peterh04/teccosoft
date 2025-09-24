ffd20f97d6111ddb43f3150cfe6da127366c04b4
Peter Heri <peterheri@Peters-MacBook-Pro.local>
2025-09-24 18:19:26 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const menuToggleBtn = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+const themeToggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+const logoImage = document.getElementById("logo-img");
+
+menuToggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
+
+themeToggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  if (body.classList.contains("dark-mode")) {
+    logoImage.src = "./assets/logo_dark.png";
+    document.querySelector(".web-icon").src =
+      "./assets/icons/laptop-solid-dark.svg";
+    document.querySelector(".globe-icon").src =
+      "./assets/icons/globe-solid-dark.svg";
+    document.querySelector(".mobile-icon").src =
+      "./assets/icons/mobile-solid-full-dark.svg";
+    document.querySelector(".server-icon").src =
+      "./assets/icons/server-solid-dark.svg";
+    document.querySelector(".tools-icon").src =
+      "./assets/icons/screwdriver-wrench-solid-dark.svg";
+    document.querySelector(".palette-icon").src =
+      "../assets/icons/palette-solid-full-dark.svg";
+  } else {
+    logoImage.src = "./assets/logo_light.png";
+  }
+});
