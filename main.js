62d1eb3109afc87a0f56973fc72fef97ca830d0e
Peter Heri <peterheri@Peters-MacBook-Pro.local>
2025-09-26 13:45:14 +0300

    feat

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const menu = document.getElementById("menu");
 const themeToggleBtn = document.getElementById("theme-toggle");
 const body = document.body;
 const logoImage = document.getElementById("logo-img");
+const themeIcon = document.getElementById("light-dark-toggle");
+const menuIcon = document.getElementById("menu-toggle-icon");
 
 menuToggleBtn.addEventListener("click", () => {
   menu.classList.toggle("show");
@@ -12,18 +14,11 @@ menuToggleBtn.addEventListener("click", () => {
 themeToggleBtn.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
   if (body.classList.contains("dark-mode")) {
-    document.querySelector(".web-icon").src =
-      "./assets/icons/laptop-solid-dark.svg";
-    document.querySelector(".globe-icon").src =
-      "./assets/icons/globe-solid-dark.svg";
-    document.querySelector(".mobile-icon").src =
-      "./assets/icons/mobile-solid-full-dark.svg";
-    document.querySelector(".server-icon").src =
-      "./assets/icons/server-solid-dark.svg";
-    document.querySelector(".tools-icon").src =
-      "./assets/icons/screwdriver-wrench-solid-dark.svg";
-    document.querySelector(".palette-icon").src =
-      "../assets/icons/palette-solid-full-dark.svg";
+    console.log(menuIcon);
+    themeIcon.src = "./assets/icons/sun-solid-full-dark.svg";
+    menuIcon.src = "./assets/icons/bars-solid-full-dark.svg";
   } else {
+    themeIcon.src = "./assets/icons/moon-solid-full.svg";
+    menuIcon.src = "./assets/icons/bars-solid-full.svg";
   }
 });
